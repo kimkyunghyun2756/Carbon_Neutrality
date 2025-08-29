804ea3e346b2017a8b1feb60e15f4fea9e0382da
kimkyunghyun2756 <pyhon7410@gmail.com>
2025-08-29 14:03:10 +0900

    rollback

--- apps/frontend/app.py
+++ apps/frontend/app.py
@@ -2,14 +2,12 @@ import streamlit as st
 import pgs.dashboard as dashboard
 import pgs.policyview as policy_view
 import pgs.manual as manual
-import pgs.csv_data as cd
 
 st.set_page_config(page_title="Carbon Dashboard", layout="wide")
 
 page = st.sidebar.radio("Pages", [
     "구축 매뉴얼",
     "데이터-정책 연계표",
-    "CSV 전체 데이터",
     "Dashboard"
 ])
 
@@ -19,5 +17,3 @@ elif page == "데이터-정책 연계표":
     policy_view.render()
 elif page == "구축 매뉴얼":
     manual.render()
-elif page == "CSV 전체 데이터":
-    cd.render_csv_page()
